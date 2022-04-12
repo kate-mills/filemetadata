9f757eee089e34581587ecd736480e92fcafe6ba
Kate Mills <helloworldloveksm@icloud.com>
2022-04-12 13:21:21 -0700

    enable 'files' upload

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+
+var fileUpload = require('express-fileupload');
+var bodyParser = require('body-parser');
+
+require('dotenv').config();
 
 var app = express();
 
 app.use(cors());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
+//enable `files` uploads
+app.use(fileUpload({ createParentPath: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
