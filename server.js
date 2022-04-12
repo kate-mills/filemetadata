fa28eb547f07669d64a02e89e9cab65e363b22f7
Kate Mills <helloworldloveksm@icloud.com>
2022-04-12 13:27:47 -0700

    prettier

--- server.js
+++ server.js
@@ -1,39 +1,41 @@
-var express = require('express')
-var cors = require('cors')
+var express = require('express');
+var cors = require('cors');
 
-var fileUpload = require('express-fileupload')
-var bodyParser = require('body-parser')
+var fileUpload = require('express-fileupload');
+var bodyParser = require('body-parser');
 
-require('dotenv').config()
+require('dotenv').config();
 
-var app = express()
+var app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use('/public', express.static(process.cwd() + '/public'))
+app.use('/public', express.static(process.cwd() + '/public'));
 
 //enable `files` uploads
-app.use(fileUpload({ createParentPath: true }))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(fileUpload({ createParentPath: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html')
-})
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 app.post('/api/fileanalyse', async (req, res) => {
   try {
     if (!req.files) {
-      res.send({ status: false, message: 'No file uploaded' })
+      res.send({ status: false, message: 'No file uploaded' });
     } else {
-      let { upfile: { name, mimetype: type, size } } = req.files
-      res.status(200).send({ name, type, size })
+      let {
+        upfile: { name, mimetype: type, size }
+      } = req.files;
+      res.status(200).send({ name, type, size });
     }
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
-})
+});
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-})
+  console.log('Your app is listening on port ' + port);
+});
